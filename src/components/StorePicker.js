56fc0ba782b50865907a8ae7a7b624fceccd28be
Neel Patel <321neel@gmail.com>
2022-05-08 01:20:38 -0400

    routing on click

--- src/components/StorePicker.js
+++ src/components/StorePicker.js
@@ -3,17 +3,24 @@ import { getFunName } from '../helpers'
 
 class StorePicker extends React.Component {
 
+
     myInput = React.createRef();
 
-    goToStore(e){
+    goToStore = (e) => {
+        //prevent default
         e.preventDefault();
-        console.log(this.myInput);
+        //get the text from input
+        console.log(this)
+        //change the page to /store/input
     }
+
+    
 
     render(){
         return (
             <form className='store-selector' onSubmit={this.goToStore}>
                 <h2>Please Enter A Store</h2>
+
                 <input type="text" ref={this.myInput} placeholder="Store Name" required defaultValue={getFunName()} />
                 <button type='submit'>Visit Store →</button>
             </form>
